d85ac5b64fcb76ea79e4c6f9863f9809bbb9ccb3
Murilooaguiar <146784737+Murilooaguiar@users.noreply.github.com>
2023-10-25 11:35:23 -0300

    atividade22

---
+++ atividade22.py
@@ -0,0 +1,5 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+
+for par  in range (50):
+    if par % 2 == 0:
+     print(par)
